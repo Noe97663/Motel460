8317552a76a04eb4e97444900f850c99046c9b65
Noel Martin Poothokaran <noelmpoothokaran@arizona.edu>
2023-04-28 14:40:13 -0700

    Drop tables before making them, if they exist

--- DatabaseInit.java
+++ DatabaseInit.java
@@ -1,7 +1,265 @@
 import java.sql.*;
 
 public class DatabaseInit {
-    public static void main(String[] args) {
+    /*---------------------------------------------------------------------
+    |  Method: establishConnection(String cmdLineArgs[])
+    |
+    |  Purpose:        Uses a username and password (from the command line
+    |                   arguments, if provided) to establish a connection to the
+    |                   oracle datatbase.
+    |
+    |
+    |  Pre-condition:  The username and password given as command line arguments 
+    |                   are valid. The JDBC driver has been added to the classpath.
+    |                   cmdLineArgs are of size 2, if custom login credentials are
+    |                   preferred.
+    |                  
+    |
+    |  Post-condition: A connection to the database has been established.
+    |
+    |  Parameters:
+    |      cmdLineArgs - an array of Strings representing the command line 
+    |                     arguments used to run Prog3. Contains login credentials
+    |
+    |  Returns:        A Connection object that enables use of the database
+    *-------------------------------------------------------------------*/
+    public static Connection establishConnection(String cmdLineArgs[]){
+        final String oracleURL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+ 
+        String username = "huyle";    // Oracle DBMS username
+        String password = "a6398";    // Oracle DBMS password
+
+        // get username/password from cmd line args
+        if (cmdLineArgs.length == 2) {
+            username = cmdLineArgs[0];
+            password = cmdLineArgs[1];
+        }
+        System.out.print("Using username-password combo of: ");
+        System.out.print(username+"-"+password);
+        System.out.println(" to access oracle database.");
+ 
+ 
+        // load the (Oracle) JDBC driver by initializing its base
+        // class, 'oracle.jdbc.OracleDriver'.
+ 
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } 
+        catch (ClassNotFoundException e) {
+            System.err.println("*** ClassNotFoundException:  "
+                + "Error loading Oracle JDBC driver.  \n"
+                + "\tPerhaps the driver is not on the Classpath?");
+            System.exit(-1);
+        }
+
+        // make and return a database connection to the user's
+        // Oracle database
+ 
         Connection dbConn = null;
+        try {
+            dbConn = DriverManager.getConnection(oracleURL,username,password);
+            }
+        catch (SQLException e) {
+            System.err.println("*** SQLException:  "
+                + "Could not open JDBC connection.");
+            System.err.println("\tMessage:   " + e.getMessage());
+            System.err.println("\tSQLState:  " + e.getSQLState());
+            System.err.println("\tErrorCode: " + e.getErrorCode());
+            System.exit(-1);
+        }
+        return dbConn;
+    }  // establihsConnection()
+
+    /*---------------------------------------------------------------------
+    |  Method: dropTables(Connection dbConn)
+    |
+    |  Purpose:        Drops the tables that may have been created previously
+    |                   when testing the program.
+    |
+    |  Pre-condition:  A valid connection has been established to the oracle 
+    |                  database through the dbConn object.
+    |
+    |  Post-condition: The required tables have been dropped from the database/
+    |
+    |  Parameters:
+    |      dbConn - A Connection object used to drop tables in the database.
+    |
+    |  Returns:   None. Drops relevant tables in the oracle database.
+    *-------------------------------------------------------------------*/
+    public static void dropTables(Connection dbConn){
+        Statement stmt = null;
+        ResultSet answer = null;
+
+        //dropping Guest table
+        try {
+            String query = "drop table HUYLE.GUEST";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.GUEST.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.GUEST cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Rating table
+        try {
+            String query = "drop table HUYLE.RATING";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.RATING.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.RATING cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping ClubMember table
+        try {
+            String query = "drop table HUYLE.CLUBMEMBER";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.CLUBMEMBER.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.CLUBMEMBER cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Booking table
+        try {
+            String query = "drop table HUYLE.BOOKING";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.BOOKING.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.BOOKING cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Transaction table
+        try {
+            String query = "drop table HUYLE.TRANSACTION";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.TRANSACTION.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.TRANSACTION cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Room table
+        try {
+            String query = "drop table HUYLE.ROOM";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.ROOM.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.ROOM cannot be dropped"
+            + " as it does not exist.");
+        }
+
+
+        //dropping RoomClassification table
+        try {
+            String query = "drop table HUYLE.ROOMCLASSIFICATION";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.ROOMCLASSIFICATION.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.ROOMCLASSIFICATION cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Employee table
+        try {
+            String query = "drop table HUYLE.EMPLOYEE";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.EMPLOYEE.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.EMPLOYEE cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Shift table
+        try {
+            String query = "drop table HUYLE.SHIFT";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.SHIFT.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.SHIFT cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        //dropping Amenity table
+        try {
+            String query = "drop table HUYLE.AMENITY";
+            stmt = dbConn.createStatement();
+            answer = stmt.executeQuery(query);
+            if (answer != null) {
+                System.out.println("\nDropped table HUYLE.AMENITY.\n");
+            }
+        } 
+        //dropping table failed.
+        catch (SQLException e) {
+            System.err.println("Table HUYLE.AMENITY cannot be dropped"
+            + " as it does not exist.");
+        }
+
+        
+
+        //closing Statement object
+        try{
+            stmt.close();
+        }
+        catch(SQLException e){
+            System.out.println("Error while closing Statement.");
+        }
+        
+    }  // dropTables()
+
+    
+
+    public static void main(String[] args) {
+        
+        Connection dbConn = establishConnection(args);
+        dropTables(dbConn);
+        System.out.println("asdf");
+
     }   
 }
