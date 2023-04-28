b2276945075e75ed71fc044ba27126f6ed7a01dd
Huy Le <huyyyle@gmail.com>
2023-04-28 14:48:25 -0700

    Small fix

--- DatabasePopulate.java
+++ DatabasePopulate.java
@@ -2,4 +2,12 @@ public class DatabasePopulate {
     public static void main(String[] args) {
         
     }
+
+    public static void populate(String CSVFile) {
+        // Read CSV file
+        // For each line, create a new object
+        // Add object to database
+    }
+
+
 }
