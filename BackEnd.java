2738a83d72a40f414545779d567d707866ea657a
Noel Martin Poothokaran <noelmpoothokaran@arizona.edu>
2023-05-03 13:43:10 -0700

    bug fixes q1

--- BackEnd.java
+++ BackEnd.java
@@ -91,12 +91,12 @@ public class BackEnd {
                 while (roomTypeAnswer.next()) {
                     // ----- GET EACH TYPE
                     roomType = roomTypeAnswer.getString("Type");     
-                    String roomPriceQuery = "Select roomPrice from RoomClassification where type = '" + roomType + "'";
+                    String roomPriceQuery = "Select Price from RoomClassification where type = '" + roomType + "'";
                     ResultSet roomPriceAnswer = stmt.executeQuery(roomPriceQuery);
                     // ----- ADD EACH (ROOM PRICE X NUM DAYS)
                     if (roomPriceAnswer != null) {
                         while (roomPriceAnswer.next()) {
-                            sum += roomPriceAnswer.getInt("roomPrice"); // * numDays          
+                            sum += roomPriceAnswer.getInt("Price"); // * numDays          
                         }
                     }
                 }
