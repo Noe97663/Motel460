b124d74fe06401ba1babdeb280603fe28bed6bd7
Mike Yu <mike051515@gmail.com>
2023-05-03 19:29:24 -0700

    input fixed

--- FrontEnd.java
+++ FrontEnd.java
@@ -263,17 +263,18 @@ public class FrontEnd {
             System.out.println("Enter 1/2/3/4/5/6/7/8 or -1 to exit: ");
             ResultSet ans;
             int quesNum = scn.nextInt();  // Read user input
-            scn.nextLine();
             if (quesNum == 1){
                 System.out.println("Enter the BookingID Associated with the Bill: ");
                 int bID = scn.nextInt();
                 backEnd.query1(bID);
                 // print data...
             } else if (quesNum == 2){
+                scn.nextLine();
                 System.out.println("Enter the date in this format: YYYY-MM-DD");
                 String date = scn.nextLine();
                 ans = backEnd.query2(date);
             } else if (quesNum == 3) {
+                scn.nextLine();
                 System.out.println("Enter the week: "); // from?
                 String week = scn.nextLine();
                 ans = backEnd.query3(week);
